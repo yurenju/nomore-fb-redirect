e2f6193234251cf33240865bc961bcec5359971a
Yuren Ju <yurenju@gmail.com>
2014-01-12 00:49:47 +0800

    Support mobile device

--- data/fblink.js
+++ data/fblink.js
@@ -1,21 +1,25 @@
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+const RE_REDIRECT = new RegExp('[?|&]u=([^&;]+?)(&|#|;|$)');
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var observer = new MutationObserver(function(mutations, observer) {
-  var links = document.querySelectorAll('a[rel=nofollow]');
+  var links = (!navigator.userAgent.contains('Mobile'))
+    ? document.querySelectorAll('a[rel=nofollow]')
+    : document.querySelectorAll('a[target=_blank]');
 
   Array.prototype.forEach.call(links, function(link) {
-    if (!link.href.contains('www.facebook.com/l.php')) {
+    if (!link.href.contains('facebook.com/l.php')) {
       return;
     }
 
-    var matched = link.search.match(new RegExp('[?|&]u=([^&;]+?)(&|#|;|$)'));
+    var matched = link.search.match(RE_REDIRECT);
     if (matched) {
       link.removeAttribute('onclick');
       link.href = decodeURIComponent(matched[1]);
     }
   });
-})
+});
 
 observer.observe(document, {
   subtree: true,
   attributes: true
-})
+});
